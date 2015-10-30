215dc3f5d5fb0f1b98d3dbbb1bc3d046bb1c449c
Adam Sandberg Eriksson <adam@sandbergericsson.se>
2015-10-30 13:34:59 +0100

    Power series: allow finite power series

--- exploration/PowerSeries.hs
+++ exploration/PowerSeries.hs
@@ -1,5 +1,9 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 -- | Power series, code from Power Series, Power Serious (M. Douglas McIlroy)
 
 module PowerSeries where
@@ -10,35 +14,54 @@ import GHC.Exts
 
 default (Integer, Rational, Double)
 
-data PS a = Cons a (PS a)
 
-showPS :: Show a => PS a -> String
-showPS fs = go 20 fs
-  where go 0 _ = "..."
-        go n (Cons f fs) = show f ++ ", " ++ go (pred n) fs
+newtype PS a = PS {unPS :: [a]}
+  deriving (Functor)
 
-zipWithPS f (Cons a as) (Cons b bs) = Cons (f a b) (zipWithPS f as bs)
+-- data PS a = Cons a (PS a)
+
+uncons :: Num a => PS a -> (a, PS a)
+uncons (PS (a:as)) = (a, PS as)
+uncons (PS _) = (0, PS [])
+
+cons a (PS as) = PS (a:as)
+
+pattern (:.) f fs <- (uncons -> (f, fs))
+  where (:.) f fs = cons f fs
+
+infixr 5 :.
+
+x = PS [0,1]
+ps0 = PS []
+
+-- showPS :: Show a => PS a -> String
+-- showPS fs = go 20 fs
+--   where go 0 _ = "..."
+--         go n (Cons f fs) = show f ++ ", " ++ go (pred n) fs
+
+zipWithPS :: (a -> b -> c) -> PS a -> PS b -> PS c
+zipWithPS f (PS as) (PS bs) = PS (zipWith f as bs)
 
 -- to use list syntax for power series
 instance Num a => IsList (PS a) where
   type Item (PS a) = a
-  fromList = foldr Cons 0
-  toList (Cons f fs) = f : toList fs
-
-instance Functor PS where
-  fmap f (Cons a as) = Cons (f a) (fmap f as)
+  fromList = PS
+  toList (PS l) = l
 
 instance Applicative PS where
-  pure a = let p = Cons a p in p
+  pure a = let p = a:p in PS p
   fs <*> as = zipWithPS ($) fs as
 
 instance Num a => Num (PS a) where
-  fromInteger n = Cons (fromInteger n) (pure 0)
+  fromInteger n = cons (fromInteger n) ps0
   negate = fmap negate
   abs = fmap abs
   signum = fmap signum
   (+) = liftA2 (+)
-  (Cons f fs) * (Cons g gs) = Cons (f * g) (f .* gs + fs * (Cons g gs))
+  (uncons -> (f, fs)) * (uncons -> (g, gs)) =
+    cons (f * g) (f .* gs + fs * (cons g gs))
+
+--  (Cons f fs) * (Cons g gs) = Cons (f * g) (f .* gs + fs * (Cons g gs))
 
 infixl 7 .*
 -- | multiply with scalar
@@ -46,29 +69,30 @@ infixl 7 .*
 a .* ps = (*) <$> pure a <*> ps
 
 instance (Eq a, Fractional a) => Fractional (PS a) where
-  fromRational r = Cons (fromRational r) (pure 0)
-  (Cons f fs) / (Cons g gs)
+  fromRational r = cons (fromRational r) ps0
+  (uncons -> (f, fs)) / (uncons -> (g, gs))
     | f == 0
     , g == 0
     = fs / gs
     | otherwise
-    = let q = f / g in Cons q ((fs - q .* gs) / (Cons g gs))
+    = let q = f / g in cons q ((fs - q .* gs) / (cons g gs))
+
 
 -- | Composition
-compose (Cons f fs) (Cons g gs)
-  | g == 0 = Cons f (gs * (compose fs (Cons g gs)))
+compose (uncons -> (f, fs)) (uncons -> (g, gs))
+  | g == 0 = cons f (gs * (compose fs (cons g gs)))
   | otherwise = error "compose: first term not 0"
 
 -- | Reversion,
 -- Given F find R such that F(R(x)) = x
-revert (Cons f fs)
-  | f == 0    = let rs = Cons 0 (1 / (compose fs rs)) in rs
+revert (uncons -> (f, fs))
+  | f == 0    = let rs = 0 :. (1 / (compose fs rs)) in rs
   | otherwise = error "revert: first term not 0"
 
 
-deriv (Cons _ fs) = (*) <$> [1..] <*> fs
+deriv (uncons -> (_, fs)) = (*) <$> [1..] <*> fs
 
-integral fs = Cons 0 ((/) <$> fs <*> [1..])
+integral fs = cons 0 ((/) <$> fs <*> [1..])
 
 -- prop_int_deriv_id = \fs -> deriv (integral fs) == fs -- no equality on streams, bisimilarity
 
@@ -78,6 +102,11 @@ sinx = integral cosx
 cosx = 1 - integral sinx
 
 
-sqrtx (Cons 0 (Cons 0 fs)) = Cons 0 (sqrtx fs)
-sqrtx (Cons 1 fs) = let qs = 1 + integral ((deriv (Cons 1 fs)) / (2 .* qs)) in qs
+sqrtx (0 :. 0 :. fs) = 0 :. sqrtx fs
+sqrtx (1 :. fs) = let qs = 1 + integral ((deriv (1 :. fs)) / (2 .* qs)) in qs
 sqrtx _ = error "sqrtx: first terms wrong"
+
+
+-- enumeration of binary trees
+
+ts = 1 :. (ts ^ 2)
