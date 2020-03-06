65be6f0dcb110296b866403aa92c0d6c33758df5
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2020-03-06 19:55:08 +0100

    add shell.nix to build book

---
+++ L/shell.nix
@@ -0,0 +1,64 @@
+# with import (fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-18.03.tar.gz) {};
+with import (fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-19.09.tar.gz) {};
+
+stdenv.mkDerivation {
+  name = "docsEnv";
+  buildInputs = [ haskellPackages.lhs2tex
+                  # python3Packages.pygments
+                  biber
+                  # zip
+                  (texlive.combine {
+                       inherit (texlive)
+                       algorithm2e
+                       a4wide
+                       acmart
+                       biblatex
+                       boondox
+                       collection-fontsrecommended
+                       comment
+                       cleveref
+                       enumitem
+                       environ
+                       fontaxes
+                       framed
+                       fvextra
+                       ifplatform
+                       ifsym
+                       inconsolata
+                       kastrup
+                       latexmk
+                       libertine
+                       listings
+                       lm
+                       logreq
+                       mathpartir
+                       minted
+                       mweights
+                       ncclatex
+                       ncctools
+                       newtx
+                       newtxsf
+                       newtxtt
+                       newunicodechar
+                       prftree
+                       relsize
+                       scheme-small wrapfig marvosym wasysym
+                       stmaryrd
+                       lazylist polytable # lhs2tex
+                       tabu
+                       todonotes
+                       totpages
+                       tikz-cd
+                       trimspaces
+                       thmtools
+                       ucs
+                       wasy cm-super unicode-math filehook lm-math capt-of
+                       xargs
+                       xstring ucharcat
+                       xifthen
+                       ifmtarg
+                       varwidth
+                       ;
+                     })
+                ];
+}
