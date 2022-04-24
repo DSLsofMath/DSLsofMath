01847bcad6038d476900c34af241f1b5d5e022be
Patrik Jansson <patrik.ja@gmail.com>
2022-04-24 18:05:20 +0200

    tiny nix update: the book depends on the LaTeX doi package.

--- L/shell.nix
+++ L/shell.nix
@@ -63,6 +63,7 @@ stdenv.mkDerivation {
                        xifthen
                        ifmtarg
                        varwidth
+		       doi
                        ;
                      })
                 ];
